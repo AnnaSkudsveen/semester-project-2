57553caa5c58d92e27fa4e2cbe78987b675aac0d
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-11-27 13:45:12 +0100

    perf: added API paths

---
+++ src/js/api/constants.js
@@ -0,0 +1,19 @@
+import { getKey } from "./auth/key.js";
+
+export const API_KEY = "a5cebe50-8176-4f85-b1dc-180e70dfcb04";
+
+export const API_BASE = "https://v2.api.noroff.dev";
+
+export const API_AUTH = `${API_BASE}/auth`;
+
+export const API_AUTH_LOGIN = `${API_AUTH}/login`;
+
+export const API_AUTH_REGISTER = `${API_AUTH}/register`;
+
+export const API_AUTH_KEY = `${API_AUTH}/create-api-key`;
+
+export const API_SOCIAL = `${API_BASE}/social`;
+
+export const API_SOCIAL_POSTS = `${API_SOCIAL}/posts`;
+
+export const API_SOCIAL_PROFILES = `${API_SOCIAL}/profiles`;
