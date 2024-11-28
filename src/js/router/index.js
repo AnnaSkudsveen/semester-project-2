a259c901f9a01676c8c9d4a0d01dbf036ebd93ae
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-11-28 22:37:23 +0100

    fix: update profileEdit name and fixed bugs in router that caused issues

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -1,31 +1,26 @@
 export default async function router(pathname = window.location.pathname) {
+  console.log("Router function called with pathname:", pathname);
   switch (pathname) {
     case "/":
       await import("./views/home.js");
       break;
-    case "/auth/":
+    case "/html/auth/":
       await import("./views/auth.js");
       break;
-    //   case "/auth/login/":
-    //     await import("./views/login.js");
-    //     break;
-    //   case "/auth/register/":
-    //     await import("./views/register.js");
-    //     break;
-    case "/listing/":
+    case "/html/listing/":
       await import("./views/listing.js");
       break;
-    case "/listing/edit/":
+    case "/html/listing/edit/":
       await import("./views/listingEdit.js");
       break;
-    case "/listing/create/":
+    case "/html/listing/create/":
       await import("./views/listingCreate.js");
       break;
-    case "/profile/":
+    case "/html/profile/":
       await import("./views/profile.js");
       break;
-    case "/profile/edit/":
-      await import("./views/profileEdit.js");
+    case "/html/profile/edit/":
+      await import("./views/profileUpdate.js");
       break;
     default:
       await import("./views/notFound.js");
