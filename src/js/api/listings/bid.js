be1e20cdd9bcb74741d374e43a1f812b04547205
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-12-02 22:44:25 +0100

    feat: create http request to handle bid POSTs

---
+++ src/js/api/listings/bid.js
@@ -0,0 +1,43 @@
+import { API_AUCTION_POSTS } from "../constants.js";
+import { headers } from "../headers.js";
+
+const url = new URL(window.location.href);
+const params = new URLSearchParams(url.search);
+
+const listingId = params.get("id");
+console.log(listingId);
+
+export async function bidOnItem(bid) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers()
+    },
+    body: JSON.stringify({
+      amount: bid
+    })
+  };
+
+  try {
+    const response = await fetch(
+      `${API_AUCTION_POSTS}/${listingId}/bids`,
+      options
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Error", errorData);
+      throw new Error(`Error ${response.status}: ${errorData.message}`);
+    }
+
+    const data = await response.json();
+    console.log("Bid posted successfully:", data);
+
+    // window.location.href = "/";
+    return data;
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw error;
+  }
+}
