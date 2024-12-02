b82d86229bf9e9c305cee53b2bae1a28f2a131e7
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-12-02 21:34:24 +0100

    feat: created POST request for creating listings

---
+++ src/js/api/listings/create.js
@@ -0,0 +1,40 @@
+import { API_AUCTION_POSTS } from "../constants.js";
+import { headers } from "../headers.js";
+
+export async function createListing(title, media, body, endsAt, tags) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers()
+    },
+    body: JSON.stringify({
+      title: `${title}`,
+      body: `${body}`,
+      endsAt: `${endsAt}`,
+      tags: [`${tags}`],
+      avatar: {
+        url: `${media}`
+      }
+    })
+  };
+
+  try {
+    const response = await fetch(`${API_AUCTION_POSTS}`, options);
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Error posting listing", errorData);
+      throw new Error(`Error ${response.status}: ${errorData.message}`);
+    }
+
+    const data = await response.json();
+    console.log("Listing created successfully:", data);
+
+    // window.location.href = "/";
+    return data;
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw error;
+  }
+}
