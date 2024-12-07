835d59510039e4a424309d95d66476ca329cb83a
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-12-08 00:55:49 +0100

    feat: add tailwind config and postcss config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./**/*.{html,js,ts}", "!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
-
+  plugins: []
+};
