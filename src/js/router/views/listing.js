d0fc7ba30434b05bbfbbd7143409d8d6dfb54d41
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-12-13 22:04:20 +0100

    style: add styling changes to single listing

--- src/js/router/views/listing.js
+++ src/js/router/views/listing.js
@@ -2,6 +2,7 @@ import { onBidOnItem } from "../../ui/listing/bid.js";
 import { API_AUCTION_POSTS } from "../../api/constants";
 import { headers } from "../../api/headers.js";
 import { showUserCredit } from "./navBar.js";
+import { updateCountdown } from "../../api/profile/posts.js";
 
 const bidForm = document.forms.bidModal;
 const bearerToken = localStorage.getItem("bearerToken");
@@ -14,6 +15,8 @@ const username = localStorage.getItem("author");
 const listingSection = document.querySelector(".listingSection");
 const listingImages = document.querySelector(".listingImages");
 const bidsSection = document.querySelector(".bidAmounts");
+const smallImgs = document.querySelector(".smallImgs");
+const bigImg = document.querySelector(".bigImg");
 
 bidForm.addEventListener("submit", onBidOnItem);
 showUserCredit(username);
@@ -35,28 +38,69 @@ async function showListing(id) {
       options
     );
     const listingData = await response.json();
+    console.log(listingData);
 
-    listingData.data.media.forEach((mediaItem) => {
-      listingImages.innerHTML += `
-          <img src="${mediaItem.url}" alt="picture of listed item">
-        `;
-    });
+    const media = listingData.data.media;
+
+    if (!media || media.length === 0) {
+      bigImg.innerHTML = "<p>No images available</p>";
+      smallImgs.innerHTML = "";
+      return;
+    }
+
+    bigImg.innerHTML = `
+      <div class="flex justify-center">
+        <img src="${media[0].url}" alt="Main image" class="w-72 sm:w-96 md:w-[606px] lg:w-[424px]">
+      </div>
+    `;
+
+    smallImgs.innerHTML = "";
+    for (let i = 1; i < media.length; i++) {
+      smallImgs.innerHTML += `
+        <div class="">
+          <img src="${media[i].url}" alt="" class="h-20 sm:h-44 md:h-32 w-20 sm:w-44 md:w-32 object-cover">
+        </div>
+      `;
+    }
 
     listingSection.innerHTML = `
-    <section class="listing">
-          <h2>${listingData.data.title}</h2>
-          <p>${listingData.data.description}</p>
-          <p>${listingData.data.endsAt}</p>
-          <p>${listingData.data._count.bids}</p>
+    <section class="listing w-dvw p-7 break-words overflow-hidden">
+          <h2 class="text-xl font-bold overflow-hidden">${listingData.data.title}</h2>
+          <p class="overflow-hidden break-words">${listingData.data.description}</p>
+          
       </section>
+    `;
+
+    bidsSection.innerHTML = `
+    <div class="flex flex-col gap-2">
+      <p class="countdown font-bold text-alertRed-700 text-lg" data-end-date="${listingData.data.endsAt}">${listingData.data.endsAt}</p>
+      <div class="flex gap-1">
+        <p class="font-bold text-base">${listingData.data._count.bids}</p>
+        <p class="font-bold text-base">Bids on item</p>
+      </div>
+    </div>
+  
     `;
 
     listingData.data.bids.forEach((bid) => {
       bidsSection.innerHTML += `
-      <h4>${bid.bidder.name}</h4>      
-      <p>${bid.amount}</p>
+      <section class="flex gap-2">
+        <div>
+          <img src="${bid.bidder.avatar.url}" alt="Avatar" class="h-10 w-10 rounded-full object-cover">
+        </div>
+        <div>
+          <h4 class="text-sm">${bid.bidder.name}</h4>      
+          <p class="text-sm"><span class="font-bold">${bid.amount}</span> NOK</p>
+
+        </div>
+      </section>
+      
           `;
     });
+    const countdownElement = document.querySelector(".countdown");
+    const endDate = countdownElement.getAttribute("data-end-date");
+    updateCountdown(countdownElement, endDate);
+    setInterval(() => updateCountdown(countdownElement, endDate), 1000);
   }
 }
 showListing(listingId);
