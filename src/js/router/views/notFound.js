f259f3d31a0770b34dcb34b2259dc60c26e88bba
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-12-15 11:53:02 +0100

    fix: fixed error in deploying

---
+++ src/js/router/views/notFound.js
@@ -0,0 +1,1 @@
+alert("Error, page not found");
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/",
   build: {
     target: "esnext",
-    rollupOptions: {
-      external: ["../../ui/global/logout.js"],
-      output: {
-        entryFileNames: "[name].js",
-        chunkFileNames: "[name].js",
-        assetFileNames: "[name].[ext]"
-      }
+    input: {
+      main: resolve(__dirname, "./index.html"),
+      profile: resolve(__dirname, "./html/profile/index.html"),
+      editProfile: resolve(__dirname, "./html/profile/edit/index.html"),
+      listing: resolve(__dirname, "./html/listing/index.html"),
+      editListing: resolve(__dirname, "./html/listing/edit/index.html"),
+      createListing: resolve(__dirname, "./html/listing/create/index.html")
     }
   }
 });
